e07d6982da70d7fdae95fe4f5481e46504d40163
OffByOne37 <129181101+OffByOne37@users.noreply.github.com>
2023-06-05 14:21:47 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 for (let index = 0; index < 4; index++) {
-    bachelor_thesis.exampleBlock(0)
-    bachelor_thesis.exampleBlock(0)
+    bachelor_thesis.exampleBlock(0, 0)
 }
 basic.showLeds(`
     # # # # #
